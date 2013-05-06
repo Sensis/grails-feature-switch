352ba5154fc96cf1f574cede87b3e533742570f3
Travis Dixon <the.trav@gmail.com>
2013-05-06 11:50:45 +1000

    make optional param safer, use pageScope feature overrides in tagLib

--- grails-app/services/uk/co/desirableobjects/featureswitch/FeatureSwitchService.groovy
+++ grails-app/services/uk/co/desirableobjects/featureswitch/FeatureSwitchService.groovy
@@ -8,22 +8,22 @@ class FeatureSwitchService {
 
     GrailsApplication grailsApplication
 
-    boolean hasFeature(String feature, Map<String, Boolean> overrides = [:]) {
-        return overrides.containsKey(feature) ? overrides[feature] :
+    boolean hasFeature(String feature, Map<String, Boolean> overrides = null) {
+        return overrides?.containsKey(feature) ? overrides[feature] :
             (grailsApplication.config.features[feature] && grailsApplication.config.features[feature].enabled)
     }
 
-    def withFeature(String feature, Closure closure, overrides = [:]) {
+    def withFeature(String feature, Closure closure, overrides = null) {
         executeFeatureConditionally(feature, true, closure, overrides)
     }
 
-    private executeFeatureConditionally(String feature, boolean condition, Closure closure, overrides) {
+    private executeFeatureConditionally(String feature, boolean condition, Closure closure, overrides = null) {
         if (hasFeature(feature, overrides) == condition) {
             closure()
         }
     }
 
-    def withoutFeature(String feature, Closure closure, overrides = [:]) {
+    def withoutFeature(String feature, Closure closure, overrides = null) {
         executeFeatureConditionally(feature, false, closure, overrides)
     }
 
